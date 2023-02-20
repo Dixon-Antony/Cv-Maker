e2ac277a1e0011c587a56437da6a08393e73780f
Dixon-Antony <dixon.j.antony2001@gmail.com>
2023-02-20 23:52:56 +0530

    almost

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,13 @@
 
 function previewFile() {
-    var preview = document.querySelector('img');
+    var preview = document.getElementById('fimg');
     var file    = document.querySelector('input[type=file]').files[0];
+    var templateImg = document.getElementById('timg');
     var reader  = new FileReader();
   
     reader.onloadend = function () {
       preview.src = reader.result;
+      templateImg.src = reader.result;
     }
   
     if (file) {
@@ -14,3 +16,20 @@ function previewFile() {
       preview.src = "assets/img/default.jpg";
     }
   }
+
+
+
+
+    window.onload = () =>{
+      var gen = document.getElementById('generate');
+      gen.addEventListener('click',function(e){
+        e.preventDefault();
+        var template = document.querySelector('.template-container');
+        template.classList.toggle('show-template');
+        gen.style.pointerEvents='none';
+        gen.style.backgroundColor='#222';
+      });
+    };
+
+
+  
